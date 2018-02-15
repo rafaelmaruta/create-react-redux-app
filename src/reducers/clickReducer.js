9dafff8513ab812f3d9d5787c141f7f6b49e517b
rafaelmaruta <rafaelmaruta@gmail.com>
2018-02-15 14:13:44 -0200

    Spread state in reducer

--- src/reducers/clickReducer.js
+++ src/reducers/clickReducer.js
@@ -7,7 +7,10 @@ const initialState = {
 export const clickReducer = (state = initialState, action) => {
   switch (action.type) {
     case CLICK_UPDATE_VALUE:
-      return { newValue: action.newValue };
+      return {
+        ...state,
+        newValue: action.newValue
+      };
     default:
       return state;
   }
